63872febc141d61dec7dd0742f924fab552e0131
JMARR0W <83468490+JMARR0W@users.noreply.github.com>
2024-09-30 18:28:33 -0700

    added webcam support w/ opencv

---
+++ scripts/utils.py
@@ -0,0 +1,35 @@
+
+def coordFormatFromPredictions(results):
+    formattedList = []
+    for result in results[0].predictions:
+        formattedList.append([result.x, result.y, result.class_name])
+    return formattedList
+
+def formatOnlyColourVals(coordsArray):
+    formatedList = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
+    for i in range(6):
+        for k in range(7):
+            if coordsArray[i][k][2] == 'Empty':
+                formatedList[i][k] = 0
+            elif coordsArray[i][k][2] == 'Red':
+                formatedList[i][k] = 1
+            else:
+                formatedList[i][k] = 2
+    return formatedList
+
+import cv2 as cv
+
+def imageCapture():
+    capture = cv.VideoCapture(0)
+
+    if not capture.isOpened():
+        print("webcam error")
+
+    ret, frame = capture.read()
+
+    if ret:
+        cv.imwrite("webcam_photo.jpg", frame)
+
+    capture.release()
+
+imageCapture()
