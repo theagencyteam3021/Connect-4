6a9970cd78dffec34df7d3e2c30b280d2d2ace1f
JMARR0W <83468490+JMARR0W@users.noreply.github.com>
2024-10-07 18:47:33 -0700

    made code work with inference

--- main.py
+++ main.py
@@ -1,29 +1,50 @@
 from scripts.board import Board
 
-from scripts.utils import coordFormatFromPredictions, formatOnlyColourVals
+from scripts.utils import coordFormatFromPredictions, formatOnlyColourVals, imageCapture
 
-fakeArray = [
-  [5, 5, ""], [5, 10, " "], [5, 15, " "], [5, 20, " "], [5, 25, " "], [5, 30, " "],
-  [10, 5, " "], [10, 10, " "], [10, 15, " "], [10, 20, " "], [10, 25, " "], [10, 30, " "], 
-  [15, 5, " "], [15, 10, " "], [15, 15, " "], [15, 20, " "], [15, 25, " "], [15, 30, " "], 
-  [20, 5, " "], [20, 10, " "], [20, 15, " "], [20, 20, " "], [20, 25, " "], [20, 30, " "], 
-  [25, 5, " "], [25, 10, " "], [25, 15, " "], [25, 20, " "], [25, 25, " "], [25, 30, " "], 
-  [30, 5, " "], [30, 10, " "], [30, 15, " "], [30, 20, " "], [30, 25, " "], [30, 30, " "], 
-  [35, 5, " "], [35, 10, " "], [35, 15, " "], [35, 20, " "], [35, 25, " "], [35, 30, " "]
-]
-# array = coordFormatFromPredictions(results)
+from visualization.gridBoardTest import GridBoardDisplay
 
-# board2 = Board(array)
+import inference
 
-# matrix2 = board2.findMatrix()
 
-# print(matrix2)
+# from PIL import Image
 
-board = Board(fakeArray)
+imageCapture()
+
+model = inference.get_model("connect4-lxv2j/2", "fxXBp7IHZMUOlxGJbueP")
+results = model.infer(image="/webcam_image.jpg")
+
+array = coordFormatFromPredictions(results)
+
+board = Board(array)
 
 matrix = board.findMatrix()
 
 print(matrix)
+
+colorOnlyMatrix = formatOnlyColourVals(matrix)
+
+disp = GridBoardDisplay(colorOnlyMatrix)
+disp.run()
+
+
+
+
+# fakeArray = [
+#   [5, 5, ""], [5, 10, " "], [5, 15, " "], [5, 20, " "], [5, 25, " "], [5, 30, " "],
+#   [10, 5, " "], [10, 10, " "], [10, 15, " "], [10, 20, " "], [10, 25, " "], [10, 30, " "], 
+#   [15, 5, " "], [15, 10, " "], [15, 15, " "], [15, 20, " "], [15, 25, " "], [15, 30, " "], 
+#   [20, 5, " "], [20, 10, " "], [20, 15, " "], [20, 20, " "], [20, 25, " "], [20, 30, " "], 
+#   [25, 5, " "], [25, 10, " "], [25, 15, " "], [25, 20, " "], [25, 25, " "], [25, 30, " "], 
+#   [30, 5, " "], [30, 10, " "], [30, 15, " "], [30, 20, " "], [30, 25, " "], [30, 30, " "], 
+#   [35, 5, " "], [35, 10, " "], [35, 15, " "], [35, 20, " "], [35, 25, " "], [35, 30, " "]
+# ]
+
+# board = Board(fakeArray)
+
+# matrix = board.findMatrix()
+
+# print(matrix)
 # """
 # [[[5, 30, ' '], [10, 30, ' '], [15, 30, ' '], [20, 30, ' '], [25, 30, ' '], [30, 30, ' '], [35, 30, ' ']], 
 #  [[5, 25, ' '], [10, 25, ' '], [15, 25, ' '], [20, 25, ' '], [25, 25, ' '], [30, 25, ' '], [35, 25, ' ']], 
