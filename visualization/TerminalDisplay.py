09dc55ddba6bb40d946406a7485e27ab7c2ce30e
JMARR0W <83468490+JMARR0W@users.noreply.github.com>
2024-11-05 11:59:54 -0800

    console dispaly created

---
+++ visualization/TerminalDisplay.py
@@ -0,0 +1,27 @@
+
+
+sampleArray = [[1, 1, 1, 2, 1, 2, 2], [1, 1, 2, 1, 2, 2, 1], [1, 2, 1, 1, 2, 1, 2], [2, 1, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
+
+
+
+class TerminalBoard():
+    def __init__(self, array="none") -> None:
+        if array == "none":
+            print("No array provided; sample array is being used.")
+            self.array = sampleArray
+        else:
+            self.array = array
+        self.array.reverse()
+    
+    def printBoard(self):
+        for row in range(6):
+            for col in range(7):
+                if self.array[row][col] == 2:
+                    print("\033[33m●\033[0m", end=" ") 
+                elif self.array[row][col] == 1:
+                    print("\033[31m●\033[0m", end=" ") 
+                else:
+                    print("\033[38;5;244m●\033[0m", end=" ")
+            print()
+board = TerminalBoard()
+board.printBoard()
