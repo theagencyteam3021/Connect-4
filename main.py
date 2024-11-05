c112a9f18b69bbf72abab130f6277dc094529e07
JMARR0W <83468490+JMARR0W@users.noreply.github.com>
2024-11-05 11:58:25 -0800

    cleaned up main and added threading

--- main.py
+++ main.py
@@ -8,56 +8,66 @@ from alg.connect_4_alg_stolen import pick_best_move
 
 import numpy as np
 
+import threading
+
 from PIL import Image
 
 import inference
 
 
-# from PIL import Image
-
 # retakes photo until 42 detections are made
-isFirstTry = False
 
-imageCapture()
-image = Image.open("webcam_photo.jpg")
-model = inference.get_model("connect4-lxv2j/15", "fxXBp7IHZMUOlxGJbueP")
-results = model.infer(image=image)
-
-while len(results[0].predictions) != 42:
-
-    if not isFirstTry:
-        print(f"Retaking photo {len(results[0].predictions)} spots detected.")
-    else:
-        isFirstTry = False
+def getPredictionsUntilValid(file="webcam_photo.jpg", model=15):
 
     imageCapture()
-    image = Image.open("webcam_photo.jpg")
+    image = Image.open(file)
+    model = inference.get_model("connect4-lxv2j/15", "fxXBp7IHZMUOlxGJbueP")
     results = model.infer(image=image)
+    
+    while len(results[0].predictions) != 42:
+        print(f"Retaking photo {len(results[0].predictions)} spots detected.")
+        imageCapture()
+        image = Image.open(file)
+        results = model.infer(image=image)
+    return results
 
-array = coordFormatFromPredictions(results)
+def worker():
+        disp = GridBoardDisplay(colorOnlyMatrix)
+        disp.run()
 
-board = Board(array)
+        
+if __name__ == "__main__":
 
-matrix = board.findMatrix()
+    results = getPredictionsUntilValid()
 
-print(matrix)
+    array = coordFormatFromPredictions(results)
 
-colorOnlyMatrix = formatOnlyColourVals(matrix)
-                        
-disp = GridBoardDisplay(colorOnlyMatrix)
-disp.run()
+    board = Board(array)
 
-print(colorOnlyMatrix)
+    matrix = board.findMatrix()
 
-numpy_array = np.array(colorOnlyMatrix)
-print(pick_best_move(numpy_array, 1))
-
-
+    # print(matrix)
+    colorOnlyMatrix = formatOnlyColourVals(matrix)
 
 
+    threads = []
+    t = threading.Thread(target=worker)
+    threads.append(t)
+    t.start()
+
+    print(pick_best_move(colorOnlyMatrix, 1))
+
+    for t in threads:
+        t.join()
+
+    print("Main thread: All threads finished")
 
 
 
+
+
+# sampleArray = [[1, 1, 1, 2, 1, 2, 2], [1, 1, 2, 1, 2, 2, 1], [1, 2, 1, 1, 2, 1, 2], [0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
+# print(pick_best_move(sampleArray, 1))
 
 # fakeArray = [
 #   [5, 5, ""], [5, 10, " "], [5, 15, " "], [5, 20, " "], [5, 25, " "], [5, 30, " "],
@@ -68,17 +78,3 @@ print(pick_best_move(numpy_array, 1))
 #   [30, 5, " "], [30, 10, " "], [30, 15, " "], [30, 20, " "], [30, 25, " "], [30, 30, " "], 
 #   [35, 5, " "], [35, 10, " "], [35, 15, " "], [35, 20, " "], [35, 25, " "], [35, 30, " "]
 # ]
-
-# board = Board(fakeArray)
-
-# matrix = board.findMatrix()
-
-# print(matrix)
-# """
-# [[[5, 30, ' '], [10, 30, ' '], [15, 30, ' '], [20, 30, ' '], [25, 30, ' '], [30, 30, ' '], [35, 30, ' ']], 
-#  [[5, 25, ' '], [10, 25, ' '], [15, 25, ' '], [20, 25, ' '], [25, 25, ' '], [30, 25, ' '], [35, 25, ' ']], 
-#  [[5, 20, ' '], [10, 20, ' '], [15, 20, ' '], [20, 20, ' '], [25, 20, ' '], [30, 20, ' '], [35, 20, ' ']], 
-#  [[5, 15, ' '], [10, 15, ' '], [15, 15, ' '], [20, 15, ' '], [25, 15, ' '], [30, 15, ' '], [35, 15, ' ']], 
-#  [[5, 10, ' '], [10, 10, ' '], [15, 10, ' '], [20, 10, ' '], [25, 10, ' '], [30, 10, ' '], [35, 10, ' ']], 
-#  [[5, 5, ''], [10, 5, ' '], [15, 5, ' '], [20, 5, ' '], [25, 5, ' '], [30, 5, ' '], [35, 5, ' ']]]
-# """
