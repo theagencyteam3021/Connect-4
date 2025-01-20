e7f06dbaeee30ac77a0782859989ec8e2b701bf6
jmarr0w <wittjm00@gmail.com>
2025-01-20 15:48:53 -0800

    added floater detection

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,85 +1,97 @@
 
+# # def coordFormatFromPredictions(results):
+# #     formattedList = []
+# #     for result in results[0].boxes.cls:
+# #         formattedList.append([result.x, result.y, result.class_name])
+# #     return formattedList
+
 # def coordFormatFromPredictions(results):
 #     formattedList = []
-#     for result in results[0].boxes.cls:
-#         formattedList.append([result.x, result.y, result.class_name])
+#     for i in range(len(results[0].boxes.cls)):
+#         formattedList.append([float(results[0].boxes.xywh[i][0]),float(results[0].boxes.xywh[i][1]), results[0].names[int(results[0].boxes.cls[i])]])
 #     return formattedList
-
-def coordFormatFromPredictions(results):
-    formattedList = []
-    for i in range(len(results[0].boxes.cls)):
-        formattedList.append([float(results[0].boxes.xywh[i][0]),float(results[0].boxes.xywh[i][1]), results[0].names[int(results[0].boxes.cls[i])]])
-    return formattedList
     
 
 
-def formatOnlyColourVals(coordsArray):
-    formatedList = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
-    for i in range(6):
-        for k in range(7):
-            if coordsArray[i][k][2] == 'Empty':
-                formatedList[i][k] = 0
-            elif coordsArray[i][k][2] == 'Red':
-                formatedList[i][k] = 1
-            else:
-                formatedList[i][k] = 2
-    return formatedList
+# def formatOnlyColourVals(coordsArray):
+#     formatedList = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
+#     for i in range(6):
+#         for k in range(7):
+#             if coordsArray[i][k][2] == 'Empty':
+#                 formatedList[i][k] = 0
+#             elif coordsArray[i][k][2] == 'Red':
+#                 formatedList[i][k] = 1
+#             else:
+#                 formatedList[i][k] = 2
+#     return formatedList
 
-import cv2 as cv
+# import cv2 as cv
 
-def imageCapture():
-    capture = cv.VideoCapture(0)
+# def imageCapture():
+#     capture = cv.VideoCapture(0)
 
-    if not capture.isOpened():
-        print("webcam error")
+#     if not capture.isOpened():
+#         print("webcam error")
 
-    capture.set(cv.CAP_PROP_FOURCC,cv.VideoWriter_fourcc('M','J','P','G'))
+#     capture.set(cv.CAP_PROP_FOURCC,cv.VideoWriter_fourcc('M','J','P','G'))
 
-    ret, frame = capture.read()
+#     ret, frame = capture.read()
 
-    if ret:
-        cv.imwrite("webcam_photo.jpg", frame)
+#     if ret:
+#         cv.imwrite("webcam_photo.jpg", frame)
 
-    # capture.release()
+#     # capture.release()
 
-from ultralytics import YOLO
+# from ultralytics import YOLO
 
-import time
+# import time
 
-# capture and repreat until 42 detections are made
-# once detectons are made returns the results of the object detection
+# # capture and repreat until 42 detections are made
+# # once detectons are made returns the results of the object detection
 
-def getPredictionsUntilValid(file="webcam_photo.jpg", model=15):
+# def getPredictionsUntilValid(file="webcam_photo.jpg", model=15):
 
-    capture = cv.VideoCapture(0)
+#     capture = cv.VideoCapture(0)
 
-    if not capture.isOpened():
-        print("webcam error")
+#     if not capture.isOpened():
+#         print("webcam error")
 
-    capture.set(cv.CAP_PROP_FOURCC,cv.VideoWriter_fourcc('M','J','P','G'))
+#     capture.set(cv.CAP_PROP_FOURCC,cv.VideoWriter_fourcc('M','J','P','G'))
 
-    ret, frame = capture.read()
+#     ret, frame = capture.read()
 
-    if ret:
-        cv.imwrite("webcam_photo.jpg", frame)
+#     if ret:
+#         cv.imwrite("webcam_photo.jpg", frame)
 
-    model = YOLO('best.pt')
-    results = model('webcam_photo.jpg')
-    # model = inference.get_model("connect4-lxv2j/15", "fxXBp7IHZMUOlxGJbueP")
-    # results = model.infer(image=image)
+#     model = YOLO('best.pt')
+#     results = model('webcam_photo.jpg')
+#     # model = inference.get_model("connect4-lxv2j/15", "fxXBp7IHZMUOlxGJbueP")
+#     # results = model.infer(image=image)
     
-    # while len(results[0].predictions) != 42:
-    while len(results[0].boxes.cls) != 42:
-        print(f"Retaking photo {len(results[0].boxes.cls)} spots detected.")
+#     # while len(results[0].predictions) != 42:
+#     while len(results[0].boxes.cls) != 42:
+#         print(f"Retaking photo {len(results[0].boxes.cls)} spots detected.")
         
-        ret, frame = capture.read()
+#         ret, frame = capture.read()
 
-        if ret:
-            cv.imwrite("webcam_photo.jpg", frame)
+#         if ret:
+#             cv.imwrite("webcam_photo.jpg", frame)
 
-        results = model('webcam_photo.jpg')
-        # results = model.infer(image=image)
+#         results = model('webcam_photo.jpg')
+#         # results = model.infer(image=image)
 
-        time.sleep(0.5)
-    capture.release()
-    return results
+#         time.sleep(0.5)
+#     capture.release()
+#     return results
+
+sampleArray = [[1, 1, 1, 2, 1, 2, 2], [1, 1, 2, 1, 2, 2, 1], [1, 2, 1, 1, 2, 1, 2], [2, 1, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
+
+
+def checkFloaters(colorMatrix):
+    # colorMatrix.reverse()
+    for i in range(6):
+        for k in range(1, 5):
+            if (colorMatrix[k][i] != 0) and (colorMatrix[k-1][i] == 0):
+                return False
+    return True
+print(checkFloaters(sampleArray))
